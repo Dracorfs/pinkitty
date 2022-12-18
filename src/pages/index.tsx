26bb8892a70a8a2f17228b47497701cf6b6345af
Dracorfs <franciscosantopietro@hotmail.com>
2022-12-17 23:58:45 -0300

    FIX: sign-in button into the header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
       <header className="fixed top-0 inset-x-0 bg-[hsl(52,99%,62%)] h-auto flex items-center justify-start p-3 gap-1">
         <img src="https://img.icons8.com/color/48/null/cat_in_a_box.png" alt="Logo" className="h-8 max-h-40" />
         <p className="text-3xl font-extrabold tracking-tight text-[hsl(280,100%,70%)]">Pinkitty</p>
+        <AuthShowcase />
       </header>
       <main className="flex min-h-screen flex-col items-center justify-center bg-[hsl(267,72%,63%)]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -27,7 +28,6 @@ const Home: NextPage = () => {
             <p className="text-2xl text-[hsl(172,100%,48%)]">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
-            <AuthShowcase />
           </div>
         </div>
       </main>
@@ -46,13 +46,13 @@ const AuthShowcase: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-black">
+    <div>
+      <p className="text-center text-2xl text-[hsl(280,100%,70%)]">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
         {secretMessage && <span> - {secretMessage}</span>}
       </p>
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-white/20"
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-[hsl(280,100%,70%)] no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => signOut() : () => signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
